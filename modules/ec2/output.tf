6a31ffc7ccaf32b6fedd8c1e4695c3cd334724c1
Ivan Carcamo <ivandres73@unitec.edu>
2020-04-14 13:58:40 -0600

    deleted comments in output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,8 +1,4 @@
 output "instance_id" {
   description = "IDs of the ec2 instances in the target group"
-  # value = [
-  #   aws_instance.ec2-first.id,
-  #   aws_instance.ec2-second.id
-  # ]
   value = aws_instance.ec2-first.id
 }
