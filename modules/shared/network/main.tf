4d38dad14c8d44139c427ca7b5f0631456f53304
Hector Rosales <hector.rosales@acklenavenue.com>
2020-08-26 19:28:54 -0600

    add instance_tenancy property to VPC.

--- modules/shared/network/main.tf
+++ modules/shared/network/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_host
   enable_dns_support   = var.enable_dns_support
-  instance_tenancy     = "defaul"
+  instance_tenancy     = "default"
 
   tags = {
     Name    = var.name
