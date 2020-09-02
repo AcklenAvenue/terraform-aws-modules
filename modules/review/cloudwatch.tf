29b64a5c7c70108b700307b832d8f380489f812c
Hector Rosales <hector.rosales@acklenavenue.com>
2020-09-02 17:24:39 -0600

    added default logs retention day.

--- modules/review/cloudwatch.tf
+++ modules/review/cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "log_group" {
   name              = var.name
-  retention_in_days = var.retention_days
+  retention_in_days = 3
   tags = {
     Name    = var.name
     Project = var.project
