bfb9e97cbe14fd20009a697efdc6a444b1a37a5f
Hector Rosales <hector.rosales@acklenavenue.com>
2021-07-25 11:41:25 -0600

    Add ecs-standalone module

--- modules/ecs-standalone/output.tf
+++ modules/ecs-standalone/output.tf
@@ -1,9 +1,9 @@
-# output "cluster_name" {
-#   description = "Name of the ECS cluster"
-#   value       = module.ecs.cluster_name
-# }
+output "cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = module.ecs.cluster_name
+}
 
-# output "log_group" {
-#   description = "ECS log group"
-#   value = module.ecs.log_group
-# }
+output "log_group" {
+  description = "ECS log group"
+  value = module.ecs.log_group
+}
