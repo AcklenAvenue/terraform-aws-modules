1143f50727ce95a7d1d7eb32ee31e60752bdb95e
Diego Turcios <dturcios@MacBookPro16-dturcios.local>
2021-09-08 16:03:10 -0600

    Adding second output

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,8 +2,12 @@ output "aws_s3_bucket_id" {
   value = aws_s3_bucket.web_bucket.id
 }
 
-output "aws_route53_recor_idd" {
+output "aws_route53_record_id" {
   value = aws_route53_record.frontend-alias-dns-record.id
+}
+
+output "aws_route53_record_name" {
+  value = aws_route53_record.frontend-alias-dns-record.name
 }
 
 output "aws_cloudfront_distribution_id" {
